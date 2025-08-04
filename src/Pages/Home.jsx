b308746b6ef894fc0d250f79b2020fbc9353dbfa
Bhargav Kachhadiya <bhargavkachhadiya1988@gmail.com>
2025-08-04 11:08:16 +0530

    commit first

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div
       style={{
@@ -6,10 +9,15 @@ const Home = () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        // height: "100vh",
       }}
     >
       <h1>Home Page Component</h1>
+      <button
+        className="btn btn-outline-secondary mt-2"
+        onClick={() => navigate("/list")}
+      >
+        Go to List
+      </button>
     </div>
   );
 };
